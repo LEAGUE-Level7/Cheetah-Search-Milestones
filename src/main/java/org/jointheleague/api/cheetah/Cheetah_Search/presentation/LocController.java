90f64bf40a9b3b15e53270eb5b3d0d2c15f44241
mjfre <mjfre@aol.com>
2021-07-24 21:46:07 -0700

    Returning 404 status code if list of Results is empty.

--- src/main/java/org/jointheleague/api/cheetah/Cheetah_Search/presentation/LocController.java
+++ src/main/java/org/jointheleague/api/cheetah/Cheetah_Search/presentation/LocController.java
@@ -5,9 +5,12 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.jointheleague.api.cheetah.Cheetah_Search.repository.dto.Result;
 import org.jointheleague.api.cheetah.Cheetah_Search.service.LocService;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -30,7 +33,11 @@ public class LocController {
             @ApiResponse(code = 404, message = "Result(s) not found")
     })
     public List<Result> getResults(@RequestParam(value="q") String query){
-        return locService.getResults(query);
+        List<Result> results = locService.getResults(query);
+        if(CollectionUtils.isEmpty(results)){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Result(s) not found.");
+        }
+        return results;
     }
 
 }
