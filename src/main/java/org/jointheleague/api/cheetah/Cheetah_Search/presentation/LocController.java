374c2c2d352f91af33b47f9291c700ab20246395
mjfre <mjfre@aol.com>
2021-07-24 22:30:28 -0700

    Updating unit tests.

--- src/main/java/org/jointheleague/api/cheetah/Cheetah_Search/presentation/LocController.java
+++ src/main/java/org/jointheleague/api/cheetah/Cheetah_Search/presentation/LocController.java
@@ -3,11 +3,13 @@ package org.jointheleague.api.cheetah.Cheetah_Search.presentation;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import org.jointheleague.api.cheetah.Cheetah_Search.repository.dto.LocResponse;
+import org.jointheleague.api.cheetah.Cheetah_Search.repository.dto.Result;
 import org.jointheleague.api.cheetah.Cheetah_Search.service.LocService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class LocController {
@@ -21,12 +23,13 @@ public class LocController {
     @GetMapping("/searchLocResults")
     @ApiOperation(value = "Searches for articles matching the search term",
             notes = "Response may include multiple Result values.",
-            response = LocResponse.class,
+            response = Result.class,
             responseContainer="List")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Result(s) found")
+            @ApiResponse(code = 200, message = "Result(s) found"),
+            @ApiResponse(code = 404, message = "Result(s) not found")
     })
-    public LocResponse getResults(@RequestParam(value="q") String query){
+    public List<Result> getResults(@RequestParam(value="q") String query){
         return locService.getResults(query);
     }
 
